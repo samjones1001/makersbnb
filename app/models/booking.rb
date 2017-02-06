a94ea06122f360aee2fe88118b094315f89f0f29
Sam Jones <sam.jones494@googlemail.com>
2017-02-06 15:17:36 +0000

    fixed rspec error

---
+++ app/models/booking.rb
@@ -0,0 +1,14 @@
+class Booking
+  include DataMapper::Resource
+  # storage_names[:default] = 'booking'
+
+  belongs_to :user,          required: true
+  belongs_to :space,          required: true
+  # belongs_to :availabledate,  required: true
+
+  property :id,               Serial
+  property :hired,            Boolean,  required: true
+  property :from_date,        Date,     required: true
+  property :to_date,          Date,     required: true
+  property :created_on,       DateTime, required: true
+end
--- spec/feature/sign_up_spec.rb
+++ spec/feature/sign_up_spec.rb
@@ -2,6 +2,9 @@
 require 'spec_helper'
 
 feature 'User sign up' do
-
+	scenario 'visit page' do
+		visit '/users/new'
+		expect(page).to have_content('Please sign up for a MakersBnB account')
+	end
   
 end
