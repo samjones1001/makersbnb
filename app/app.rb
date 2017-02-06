7f7c030bd05bbf7913dbedf9de78214cbcaa6398
Sam Jones <sam.jones494@googlemail.com>
2017-02-06 15:37:11 +0000

    implementing sign up

--- app/app.rb
+++ app/app.rb
@@ -9,5 +9,14 @@ class Server < Sinatra::Base
     'Hello'
   end
 
+  get '/users/new' do
+  	@user = User.new
+  	erb :'users/new'
+  end
+
+  post 'users' do
+  	@user.create()
+  end
+
   run! if app_file == $0
 end
---
+++ spec/feature/web_helper.rb
@@ -0,0 +1,9 @@
+def sign_up
+	visit '/users/new'
+    fill_in 'name', with: "Joe Bloggs"
+    fill_in 'email', with: "test@email.com"
+    fill_in 'username', with: "username"
+    fill_in 'password', with: 'password'
+    fill_in 'password_confirm', with: 'password'
+    click_button 'Sign Up!'
+end
