26527dac866143d66f12b79d19f36d8bb5d08f13
Sam Jones <sam.jones494@googlemail.com>
2017-02-06 21:21:45 +0000

    signup implemented

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ require 'bcrypt'
 class User
   
   attr_reader 	:password
-  attr_accessor	:password_confirm
+  attr_accessor	:password_confirmation
 
   include DataMapper::Resource
 
@@ -19,7 +19,8 @@ class User
   property :password_digest,  Text,   required: true
 
   validates_confirmation_of :password
-
+  validates_format_of :email, as: :email_address
+  
   def password=(password)
   	@password = password
     self.password_digest = BCrypt::Password.create(password)
--- spec/feature/web_helper.rb
+++ spec/feature/web_helper.rb
@@ -1,14 +1,14 @@
-def sign_up(email: 'test@email.com',
-              name: 'Testy McTest',
-              user_name: 'Test1',
-              password: '123456',
-              password_confirm: '123456')
+# def sign_up(email: 'test@email.com',
+#               name: 'Testy McTest',
+#               user_name: 'Test1',
+#               password: '123456',
+#               password_confirm: '123456')
 
-  visit '/users/new'
-  fill_in :email,                 with: email
-  fill_in :name,                  with: name
-  fill_in :username,             with: user_name
-  fill_in :password,              with: password
-  fill_in :password_confirm,  with: password_confirm
-  click_button 'Sign Up!'
-end
+#   visit '/users/new'
+#   fill_in :email,                 with: email
+#   fill_in :name,                  with: name
+#   fill_in :username,             with: user_name
+#   fill_in :password,              with: password
+#   fill_in :password_confirm,  with: password_confirm
+#   click_button 'Sign Up!'
+# end
