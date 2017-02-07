31a5d49d1ef5a3226c765ba65c3c45e309a97983
Justin Butwell <jbutwell@hotmail.com>
2017-02-07 16:27:23 +0000

    Update app.rb

--- app/app.rb
+++ app/app.rb
@@ -15,6 +15,10 @@ class Server < Sinatra::Base
     def current_user
       @current_user ||= User.get(session[:user_id])
     end
+    
+      def format_date(date)
+      DateTime.parse(date).strftime('%d-%m-%Y')
+    end
   end
 
   get '/' do
