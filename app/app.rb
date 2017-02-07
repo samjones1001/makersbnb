d563e6e188874ddf4f2a54f909ce17488dff3363
Sam Jones <sam.jones494@googlemail.com>
2017-02-07 13:07:12 +0000

    sign in and sign out complete

--- app/app.rb
+++ app/app.rb
@@ -9,6 +9,7 @@ class Server < Sinatra::Base
   enable :sessions
   set :session_secret, 'super secret'
   register Sinatra::Flash
+  use Rack::MethodOverride
 
   get '/' do
     
@@ -27,11 +28,31 @@ class Server < Sinatra::Base
                  password_confirmation: params[:password_confirmation])
   	if @user.save
   		session[:user_id] = @user.id
-  		redirect to('/users/test')
+  		redirect to('/users/new')
   	else
   		flash.now[:errors] = @user.errors.full_messages
   		erb :'users/new'
   	end
+  end
+
+  get '/sessions/new' do
+    erb :'sessions/new'
+  end
+
+  post '/sessions' do
+    user = User.authenticate(params[:email], params[:password])
+    if user
+      session[:user_id] = user.id
+      redirect to('/sessions/new')
+    else
+      flash.now[:errors] = ['The email or password is incorrect']
+      erb :'/sessions/new'
+    end
+  end
+
+  delete '/sessions' do
+    session[:user_id] = nil
+    redirect to 'sessions/new'
   end
 
   helpers do
