5a861e3bc0b7a45957eb0ee01c5943c96908fba6
Basile Koko <basile_koko@hotmail.com>
2017-02-09 11:19:33 +0000

    user can confirm or deny booking

--- app/controllers/request_controller.rb
+++ app/controllers/request_controller.rb
@@ -18,8 +18,16 @@ class Server < Sinatra::Base
   end
 
   get '/request' do
+    my_spaces_ids = []
     @bookings = Booking.all(:user_id => session[:user_id])
+    @spaces = Space.all(:user_id => session[:user_id])
+    @spaces.each do |space|
+    my_spaces_ids << space.id
+    end
+    @my_booking_request = Booking.all(:space_id => my_spaces_ids)
     erb :'request/index'
   end
-  
+
+
+
 end
--- app/helpers.rb
+++ app/helpers.rb
@@ -10,4 +10,18 @@ module Helpers
   def format_price(price)
     '%.2f' % price
   end
+
+  def user_data(user_id)
+   User.first(:id => user_id)
+  end
+
+  def booking_data(booking_id)
+    Booking.first(:id => booking_id)
+  end
+
+  def space_data(space_id)
+    Space.first(:id => space_id)
+  end
+
+
 end
